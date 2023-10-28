de58045918b575b3d99d7194ac8368846ac7b2aa
Henok <henokmicrometre@gmail.com>
2023-10-28 09:23:39 +0100

    added node

---
+++ sse-server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/status', (request, response) => response.json({clients: clients.length}));
+
+const PORT = 3000;
+
+let clients = [];
+let facts = [];
+
+app.listen(PORT, () => {
+  console.log(`Facts Events service listening at http://localhost:${PORT}`)
+})
